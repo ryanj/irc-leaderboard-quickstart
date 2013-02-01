c915af00319ccf3b718224085810044daf2520e6
Ryan Jarvinen <ryan@ryanjarvinen.com>
2013-01-31 16:24:56 -0800

    adding irc npm dependency, initializing irc library, adding a few jokes

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 var express = require('express');
 var fs      = require('fs');
 
+var irc = require('irc');
+var bot = new irc.Client('chat.freenode.net', process.env.OPENSHIFT_APP_NAME || 'ircbot', {
+    channels: ['#botzoo', '#botwar'],
+    port: 8001,
+    debug: true
+});
 
 /**
  *  Define the sample application.
@@ -162,3 +168,30 @@ var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
 
+
+// Jokes 
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Know any good jokes?') > -1
+      || message.indexOf('good joke') > -1
+    ) {
+        bot.say(to, 'Knock knock!');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('who is there?') > -1
+      || message.indexOf("who's there?") > -1
+      || message.indexOf("Who's there?") > -1
+      || message.indexOf("Who is there?") > -1
+      )
+    {
+        bot.say(to, 'Doris');
+    }
+});
+bot.addListener('message', function(from, to, message) {
+    if(  message.indexOf('Doris who?') > -1
+      || message.indexOf("doris who?") > -1
+      )
+    {
+        bot.say(to, "Doris locked, that's why I'm knocking!");
+    }
+});
